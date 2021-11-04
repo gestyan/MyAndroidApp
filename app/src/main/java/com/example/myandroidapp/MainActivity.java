13e584bdd8face27be99ba80c408534fc3ef93df
Gestyan Ramadhan <gestyanramadhan@gmail.com>
2021-11-04 13:17:12 +0700

    add toast message

--- app/src/main/java/com/example/myandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showToast(View view){
+        Toast t = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void countUp(View view){}
+
+    public void countReset(View view){}
 }
